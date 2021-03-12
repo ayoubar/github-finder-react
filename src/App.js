575ab10152330ba685e1a149d000a1427df7d573
ayoub ar <ayoubarahmat9@gmail.com>
2021-03-12 19:41:07 +0100

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/layouts/NavBar';
 import SearchBar from './components/users/SearchBar';
+
+function About() {
+  return (
+    <div>
+      <h1> this is About Page</h1>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <form className="mt-5 container">
+      <h1>contact Page </h1>
+      <label> first name</label>
+      <input type="text" />
+      <label> last name</label>
+      <input type="text" />
+    </form>
+  );
+}
+
 function App() {
   const [title, setTitle] = useState('Github 2021');
-  const [links, setLinks] = useState(['Home', 'About', 'contact']);
+  const [links, setLinks] = useState([
+    { name: 'Home', path: '/' },
+    { name: 'About', path: '/about' },
+    { name: 'Contact', path: '/contact' },
+  ]);
 
   return (
-    <>
+    <Router>
       <NavBar
         titre={title}
         classicon={'fab fa-github me-3'}
         liens_vers_navbar={links}
       />
-      <SearchBar />
-    </>
+      <Switch>
+        <Route exact path="/">
+          <SearchBar />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/layouts/Links.js
+++ src/components/layouts/Links.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Links(props) {
   console.log('Links props', props);
@@ -6,9 +7,9 @@ function Links(props) {
     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
       {props.liens_vers_links.map((x, index) => (
         <li className="nav-item" key={index}>
-          <a className="nav-link active" aria-current="page" href="#">
-            {x}
-          </a>
+          <Link className="nav-link active" aria-current="page" to={x.path}>
+            {x.name}
+          </Link>
         </li>
       ))}
     </ul>
